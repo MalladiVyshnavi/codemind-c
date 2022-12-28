42001bee0a57c7124fc50ddd54e222dac31c4d14
MalladiVyshnavi <119839748+MalladiVyshnavi@users.noreply.github.com>
2022-12-28 19:51:41 +0530

    #include<stdio.h> int main() {     int a,b,i,c=0;     scanf("%d",&a);     for(i=a;i>0;)     {         b=i%10;         c=(c*10)+b;         i=i/10;     }     if(a==c)     printf("True");     else     printf("False"); }

---
+++ Palindrome.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,c=0;
+    scanf("%d",&a);
+    for(i=a;i>0;)
+    {
+        b=i%10;
+        c=(c*10)+b;
+        i=i/10;
+    }
+    if(a==c)
+    printf("True");
+    else
+    printf("False");
+}
